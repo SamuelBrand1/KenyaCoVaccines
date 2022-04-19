4d767845b0599fb4a5c9496d0b27e30953cd6b7f
Samuel Brand <sam055@mac.com>
2022-04-19 10:22:38 +0100

    remove older versions

--- Economic_Analysis/ICER_calculation.R
+++ Economic_Analysis/ICER_calculation.R
@@ -7,22 +7,21 @@ library(ggplot2)
 library(pastecs)
 library(dplyr)
 library(tidyverse)
-library(DescTools)
 #set the working directory
 setwd("C:/Users/corlendo/KenyaCoVaccinesPrivate")
 
 #load in all the scenario files
-load(file = "model_outputs/health_system_persp/CEA_MODEL_OUTPUT_10_scenario_2baseline_scenario_1.rda")
+load(file = "model_outputs/societal_persp/CEA_MODEL_OUTPUT_10_scenario_2baseline_scenario_1.rda")
 case.21=as.data.frame(CEA_output)
-load(file = "model_outputs/health_system_persp/CEA_MODEL_OUTPUT_10_scenario_3baseline_scenario_1.rda")
+load(file = "model_outputs/societal_persp/CEA_MODEL_OUTPUT_10_scenario_3baseline_scenario_1.rda")
 case.31=as.data.frame(CEA_output)
-load(file = "model_outputs/health_system_persp/CEA_MODEL_OUTPUT_10_scenario_4baseline_scenario_1.rda")
+load(file = "model_outputs/societal_persp/CEA_MODEL_OUTPUT_10_scenario_4baseline_scenario_1.rda")
 case.41=as.data.frame(CEA_output)
-load(file = "model_outputs/health_system_persp/CEA_MODEL_OUTPUT_10_scenario_5baseline_scenario_1.rda")
+load(file = "model_outputs/societal_persp/CEA_MODEL_OUTPUT_10_scenario_5baseline_scenario_1.rda")
 case.51=as.data.frame(CEA_output)
-load(file = "model_outputs/health_system_persp/CEA_MODEL_OUTPUT_10_scenario_6baseline_scenario_1.rda")
+load(file = "model_outputs/societal_persp/CEA_MODEL_OUTPUT_10_scenario_6baseline_scenario_1.rda")
 case.61=as.data.frame(CEA_output)
-load(file = "model_outputs/health_system_persp/CEA_MODEL_OUTPUT_10_scenario_7baseline_scenario_1.rda")
+load(file = "model_outputs/societal_persp/CEA_MODEL_OUTPUT_10_scenario_7baseline_scenario_1.rda")
 case.71=as.data.frame(CEA_output)
 
 #labels vectors
@@ -101,14 +100,8 @@ m4 <- bcea(e4,c4,ref=2,interventions=ints4,Kmax=Kmax)
 m5 <- bcea(e5,c5,ref=2,interventions=ints5,Kmax=Kmax)
 m6 <- bcea(e6,c6,ref=2,interventions=ints6,Kmax=Kmax)
 #output a summary of the results
-#summary(m1)
+summary(m1)
 
-ICER_M1 = MeanCI(data.matrix((c1[,1]-c1[,2])/(e1[,1]-e1[,2])))
-ICER_M2 = MeanCI(data.matrix((c2[,1]-c2[,2])/(e2[,1]-e2[,2])))
-ICER_M3 = MeanCI(data.matrix((c3[,1]-c3[,2])/(e3[,1]-e3[,2])))
-ICER_M4 = MeanCI(data.matrix((c4[,1]-c4[,2])/(e4[,1]-e4[,2])))
-ICER_M5 = MeanCI(data.matrix((c5[,1]-c5[,2])/(e5[,1]-e5[,2])))
-ICER_M6 = MeanCI(data.matrix((c6[,1]-c6[,2])/(e6[,1]-e6[,2])))
 
 #PLOT ALL PLOTS
 #plot health perspective
@@ -152,22 +145,134 @@ ceplane.plot(m2,wtp=919.105,xlab ="Effectiveness differential",
 
 #m3plot
 
+# ceplane.plot(m3,wtp=919.105,xlab ="Effectiveness differential",
+#              ylab ="Cost differential",
+#              title ="50% vs 70% ",pos="true",graph="base",
+#              ICER_sizes=2,
+#              xlim=c(-5e2,8e3),
+#              ylim=c(-3e7,1.4e8),
+#              cex.axis = 0.5,main="Main title")
+#   polygon(x=c(-5e2,8e3, 8e3, -5e2),
+#           y=c(-3e7, -3e7, 8e6, -2.3e6),
+#           col=rgb(0.5,0.4,0.4,0.25),border = FALSE)
+#   polygon(x=c(1e3, 8e3, 8e3, 1e3),
+#           y=c(1.15e8, 1.15e8, 1.4e8, 1.4e8),
+#           col="#FFFFFF",border = FALSE)
+#   text(x = 3e3, y = 7e7,                # Add text element
+#        "ICER = 16536.9",cex=0.8)
+#   abline(h=1.4e8)
+#
+#
+# #m4plot
+# ceplane.plot(m4,wtp=919.105,xlab ="Effectiveness differential",
+#              ylab ="Cost differential",
+#              title ="0% vs 30% (Rapid) ",pos="true",graph="base",
+#              ICER_sizes=2,
+#              xlim=c(-1e4,1.5e5),
+#              ylim=c(-8e7,3e8))
+#   polygon(x=c(-1e4,1.5e5, 1.5e5, -1e4),
+#           y=c(-8e7, -8e7, 1.4e8, -1e7),
+#           col=rgb(0.5,0.4,0.4,0.25),border = FALSE)
+#   polygon(x=c(2e4, 1.4e5, 1.4e5, 2e4),
+#           y=c(2e8, 2e8, 3e8, 3e8),
+#           col="#FFFFFF",border = FALSE)
+#   text(x = 4e4, y = 2e8,                # Add text element
+#        "ICER = 633.8",cex=0.8)
+#   abline(h=3e8)
+#
+# #m5plot
+#
+# ceplane.plot(m5,wtp=919.105,xxlab ="Effectiveness differential",
+#              ylab ="Cost differential",
+#              title ="30% vs 50% (Rapid) ",label.pos="false",graph="base",
+#              ICER_sizes=2,
+#              xlim=c(-1e3,1.2e4),
+#              ylim=c(-2e7,1e8),
+#              cex.axis = 0.5)
+#   polygon(x=c(-1e3,1.2e4, 1.2e4, -1e3),
+#           y=c(-2e7, -2e7, 1.1e7, -1.2e6),
+#           col=rgb(0.5,0.4,0.4,0.25),border = FALSE)
+# polygon(x=c(2e3, 1.15e4, 1.15e4, 2e3),
+#         y=c(8.5e7, 8.5e7, 1e8, 1e8),
+#         col="#FFFFFF",border = FALSE)
+# text(x = 4e3, y = 5e7,                # Add text element
+#      "ICER = 9131.1",cex=0.8)
+# abline(h=1e8)
+#
+# #m6plot
+# ceplane.plot(m6,wtp=919.105,xlab ="Effectiveness differential",
+#              ylab ="Cost differential",
+#              title ="50% vs 70% (Rapid)",pos="true",graph="base",
+#              ICER_sizes=2,
+#              xlim=c(-8e2,1e4),
+#              ylim=c(-2.5e7,1.4e8))
+#   polygon(x=c(-8e2,1e4, 1e4, -8e2),
+#           y=c(-2.5e7, -2.5e7, 1e7, -1.7e6),
+#           col=rgb(0.5,0.4,0.4,0.25),border = FALSE)
+# polygon(x=c(1e3, 1e4, 1e4, 1e3),
+#         y=c(1.2e8, 1.2e8, 1.4e8, 1.4e8),
+#         col="#FFFFFF",border = FALSE)
+# text(x = 4e3, y = 7e7,                # Add text element
+#      "ICER = 17640.4",cex=0.8)
+# abline(h=1.4e8)
+
+
+# #plto society perspective graphs
+par(mfrow=c(2,3))
+
+ceplane.plot(m1,wtp=919.105,
+             pos="false",
+             graph="base",
+             ICER_sizes=1,
+             xlab ="Effectiveness differential",
+             ylab ="Cost differential",
+             title ="0% vs 30% ",
+             xlim=c(-7e3,1.2e5),
+             ylim=c(-2e8,1.7e8))
+polygon(x=c(-7e3, 1.2e5, 1.2e5, -7e3),
+        y=c(-2e8, -2e8, 1.1e8, 0),
+        col=rgb(0.5,0.4,0.4,0.25),border = FALSE)
+polygon(x=c(1e4, 1.15e5, 1.15e5, 1e4),
+        y=c(1.2e8, 1.2e8, 1.7e8, 1.7e8),
+        col="#FFFFFF",border = FALSE)
+text(x = 4e4, y = -1e8,                # Add text element
+     "ICER = -812.4",cex=0.75)
+abline(h=1.7e8)
+
+#m2plot
+ceplane.plot(m2,wtp=919.105,xlab ="Effectiveness differential",
+             ylab ="Cost differential",
+             title ="30% vs 50% ",pos="true",graph="base",
+             ICER_sizes=2,
+             xlim=c(-1e3,1.5e4),
+             ylim=c(-2e7,1e8))
+polygon(x=c(-1e3, 1.5e4, 1.5e4, -1e3),
+        y=c(-2e7, -2e7, 1.4e7, -1.5e6),
+        col=rgb(0.5,0.4,0.4,0.25),border = FALSE)
+polygon(x=c(2e3, 1.45e4, 1.45e4, 2e3),
+        y=c(8e7, 8e7, 1e8, 1e8),
+        col="#FFFFFF",border = FALSE)
+text(x = 4e3, y = 5e7,                # Add text element
+     "ICER = 4033.9",cex=0.75)
+abline(h=1e8)
+
+#m3plot
+
 ceplane.plot(m3,wtp=919.105,xlab ="Effectiveness differential",
              ylab ="Cost differential",
              title ="50% vs 70% ",pos="true",graph="base",
              ICER_sizes=2,
              xlim=c(-5e2,8e3),
-             ylim=c(-3e7,1.4e8),
-             cex.axis = 0.5,main="Main title")
-  polygon(x=c(-5e2,8e3, 8e3, -5e2),
-          y=c(-3e7, -3e7, 8e6, -2.3e6),
-          col=rgb(0.5,0.4,0.4,0.25),border = FALSE)
-  polygon(x=c(1e3, 8e3, 8e3, 1e3),
-          y=c(1.15e8, 1.15e8, 1.4e8, 1.4e8),
-          col="#FFFFFF",border = FALSE)
-  text(x = 3e3, y = 7e7,                # Add text element
-       "ICER = 16536.9",cex=0.8)
-  abline(h=1.4e8)
+             ylim=c(-3e7,1.4e8))
+polygon(x=c(-1e3,8e3, 8e3, -1e3),
+        y=c(-3e7, -3e7, 8e6, -2.3e6),
+        col=rgb(0.5,0.4,0.4,0.25),border = FALSE)
+polygon(x=c(1e3, 8e3, 8e3, 1e3),
+        y=c(1.15e8, 1.15e8, 1.4e8, 1.4e8),
+        col="#FFFFFF",border = FALSE)
+text(x = 3e3, y = 7e7,                # Add text element
+     "ICER = 14615.1",cex=0.75)
+abline(h=1.4e8)
 
 
 #m4plot
@@ -176,16 +281,16 @@ ceplane.plot(m4,wtp=919.105,xlab ="Effectiveness differential",
              title ="0% vs 30% (Rapid) ",pos="true",graph="base",
              ICER_sizes=2,
              xlim=c(-1e4,1.5e5),
-             ylim=c(-8e7,3e8))
-  polygon(x=c(-1e4,1.5e5, 1.5e5, -1e4),
-          y=c(-8e7, -8e7, 1.4e8, -1e7),
-          col=rgb(0.5,0.4,0.4,0.25),border = FALSE)
-  polygon(x=c(2e4, 1.4e5, 1.4e5, 2e4),
-          y=c(2e8, 2e8, 3e8, 3e8),
-          col="#FFFFFF",border = FALSE)
-  text(x = 4e4, y = 2e8,                # Add text element
-       "ICER = 633.8",cex=0.8)
-  abline(h=3e8)
+             ylim=c(-3e8,3e8))
+polygon(x=c(-1e4,1.5e5, 1.5e5, -1e4),
+        y=c(-3e8, -3e8, 1.4e8, -1e7),
+        col=rgb(0.5,0.4,0.4,0.25),border = FALSE)
+polygon(x=c(2e4, 1.45e5, 1.45e5, 2e4),
+        y=c(2e8, 2e8, 3e8, 3e8),
+        col="#FFFFFF",border = FALSE)
+text(x = 4e4, y = -2e8,                # Add text element
+     "ICER = -1269",cex=0.75)
+abline(h=3e8)
 
 #m5plot
 
@@ -193,17 +298,17 @@ ceplane.plot(m5,wtp=919.105,xxlab ="Effectiveness differential",
              ylab ="Cost differential",
              title ="30% vs 50% (Rapid) ",label.pos="false",graph="base",
              ICER_sizes=2,
-             xlim=c(-1e3,1.2e4),
+             xlim=c(-8e2,1.2e4),
              ylim=c(-2e7,1e8),
              cex.axis = 0.5)
-  polygon(x=c(-1e3,1.2e4, 1.2e4, -1e3),
-          y=c(-2e7, -2e7, 1.1e7, -1.2e6),
-          col=rgb(0.5,0.4,0.4,0.25),border = FALSE)
+polygon(x=c(-8e2,1.2e4, 1.2e4, -8e2),
+        y=c(-2e7, -2e7, 1.1e7, -1.2e6),
+        col=rgb(0.5,0.4,0.4,0.25),border = FALSE)
 polygon(x=c(2e3, 1.15e4, 1.15e4, 2e3),
         y=c(8.5e7, 8.5e7, 1e8, 1e8),
         col="#FFFFFF",border = FALSE)
 text(x = 4e3, y = 5e7,                # Add text element
-     "ICER = 9131.1",cex=0.8)
+     "ICER = 7218.6",cex=0.75)
 abline(h=1e8)
 
 #m6plot
@@ -211,127 +316,15 @@ ceplane.plot(m6,wtp=919.105,xlab ="Effectiveness differential",
              ylab ="Cost differential",
              title ="50% vs 70% (Rapid)",pos="true",graph="base",
              ICER_sizes=2,
-             xlim=c(-8e2,1e4),
-             ylim=c(-2.5e7,1.4e8))
-  polygon(x=c(-8e2,1e4, 1e4, -8e2),
-          y=c(-2.5e7, -2.5e7, 1e7, -1.7e6),
-          col=rgb(0.5,0.4,0.4,0.25),border = FALSE)
+             xlim=c(-6e2,1e4),
+             ylim=c(-1.9e7,1.4e8))
+polygon(x=c(-6e2,1e4, 1e4, -6e2),
+        y=c(-1.9e7, -1.9e7, 1e7, -1.7e6),
+        col=rgb(0.5,0.4,0.4,0.25),border = FALSE)
 polygon(x=c(1e3, 1e4, 1e4, 1e3),
         y=c(1.2e8, 1.2e8, 1.4e8, 1.4e8),
         col="#FFFFFF",border = FALSE)
 text(x = 4e3, y = 7e7,                # Add text element
-     "ICER = 17640.4",cex=0.8)
+     "ICER = 15731",cex=0.75)
 abline(h=1.4e8)
 
-
-# # #plot societal perspective graphs
-# par(mfrow=c(2,3))
-# 
-# ceplane.plot(m1,wtp=919.105,
-#              pos="false",
-#              graph="base",
-#              ICER_sizes=1,
-#              xlab ="Effectiveness differential",
-#              ylab ="Cost differential",
-#              title ="0% vs 30% ",
-#              xlim=c(-7e3,1.2e5),
-#              ylim=c(-2e8,1.7e8))
-# polygon(x=c(-7e3, 1.2e5, 1.2e5, -7e3),
-#         y=c(-2e8, -2e8, 1.1e8, 0),
-#         col=rgb(0.5,0.4,0.4,0.25),border = FALSE)
-# polygon(x=c(1e4, 1.15e5, 1.15e5, 1e4),
-#         y=c(1.2e8, 1.2e8, 1.7e8, 1.7e8),
-#         col="#FFFFFF",border = FALSE)
-# text(x = 4e4, y = -1e8,                # Add text element
-#      "ICER = -811.8",cex=0.75)
-# abline(h=1.7e8)
-# 
-# #m2plot
-# ceplane.plot(m2,wtp=919.105,xlab ="Effectiveness differential",
-#              ylab ="Cost differential",
-#              title ="30% vs 50% ",pos="true",graph="base",
-#              ICER_sizes=2,
-#              xlim=c(-1e3,1.5e4),
-#              ylim=c(-2e7,1e8))
-# polygon(x=c(-1e3, 1.5e4, 1.5e4, -1e3),
-#         y=c(-2e7, -2e7, 1.4e7, -1.5e6),
-#         col=rgb(0.5,0.4,0.4,0.25),border = FALSE)
-# polygon(x=c(2e3, 1.45e4, 1.45e4, 2e3),
-#         y=c(8e7, 8e7, 1e8, 1e8),
-#         col="#FFFFFF",border = FALSE)
-# text(x = 4e3, y = 5e7,                # Add text element
-#      "ICER = 4039.0",cex=0.75)
-# abline(h=1e8)
-# 
-# #m3plot
-# 
-# ceplane.plot(m3,wtp=919.105,xlab ="Effectiveness differential",
-#              ylab ="Cost differential",
-#              title ="50% vs 70% ",pos="true",graph="base",
-#              ICER_sizes=2,
-#              xlim=c(-5e2,8e3),
-#              ylim=c(-3e7,1.4e8))
-# polygon(x=c(-1e3,8e3, 8e3, -1e3),
-#         y=c(-3e7, -3e7, 8e6, -2.3e6),
-#         col=rgb(0.5,0.4,0.4,0.25),border = FALSE)
-# polygon(x=c(1e3, 8e3, 8e3, 1e3),
-#         y=c(1.15e8, 1.15e8, 1.4e8, 1.4e8),
-#         col="#FFFFFF",border = FALSE)
-# text(x = 3e3, y = 7e7,                # Add text element
-#      "ICER = 14629.6",cex=0.75)
-# abline(h=1.4e8)
-# 
-# 
-# #m4plot
-# ceplane.plot(m4,wtp=919.105,xlab ="Effectiveness differential",
-#              ylab ="Cost differential",
-#              title ="0% vs 30% (Rapid) ",pos="true",graph="base",
-#              ICER_sizes=2,
-#              xlim=c(-1e4,1.5e5),
-#              ylim=c(-3e8,3e8))
-# polygon(x=c(-1e4,1.5e5, 1.5e5, -1e4),
-#         y=c(-3e8, -3e8, 1.4e8, -1e7),
-#         col=rgb(0.5,0.4,0.4,0.25),border = FALSE)
-# polygon(x=c(2e4, 1.45e5, 1.45e5, 2e4),
-#         y=c(2e8, 2e8, 3e8, 3e8),
-#         col="#FFFFFF",border = FALSE)
-# text(x = 4e4, y = -2e8,                # Add text element
-#      "ICER = -1268.5",cex=0.75)
-# abline(h=3e8)
-# 
-# #m5plot
-# 
-# ceplane.plot(m5,wtp=919.105,xxlab ="Effectiveness differential",
-#              ylab ="Cost differential",
-#              title ="30% vs 50% (Rapid) ",label.pos="false",graph="base",
-#              ICER_sizes=2,
-#              xlim=c(-8e2,1.2e4),
-#              ylim=c(-2e7,1e8),
-#              cex.axis = 0.5)
-# polygon(x=c(-8e2,1.2e4, 1.2e4, -8e2),
-#         y=c(-2e7, -2e7, 1.1e7, -1.2e6),
-#         col=rgb(0.5,0.4,0.4,0.25),border = FALSE)
-# polygon(x=c(2e3, 1.15e4, 1.15e4, 2e3),
-#         y=c(8.5e7, 8.5e7, 1e8, 1e8),
-#         col="#FFFFFF",border = FALSE)
-# text(x = 4e3, y = 5e7,                # Add text element
-#      "ICER = 7244.3",cex=0.75)
-# abline(h=1e8)
-# 
-# #m6plot
-# ceplane.plot(m6,wtp=919.105,xlab ="Effectiveness differential",
-#              ylab ="Cost differential",
-#              title ="50% vs 70% (Rapid)",pos="true",graph="base",
-#              ICER_sizes=2,
-#              xlim=c(-6e2,1e4),
-#              ylim=c(-1.9e7,1.4e8))
-# polygon(x=c(-6e2,1e4, 1e4, -6e2),
-#         y=c(-1.9e7, -1.9e7, 1e7, -1.7e6),
-#         col=rgb(0.5,0.4,0.4,0.25),border = FALSE)
-# polygon(x=c(1e3, 1e4, 1e4, 1e3),
-#         y=c(1.2e8, 1.2e8, 1.4e8, 1.4e8),
-#         col="#FFFFFF",border = FALSE)
-# text(x = 4e3, y = 7e7,                # Add text element
-#      "ICER = 15835.8",cex=0.75)
-# abline(h=1.4e8)
-# 
